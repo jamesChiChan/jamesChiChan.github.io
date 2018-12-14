3f4a7a5157334a265d872b7f9c05c1904478f7bc
jamesChiChan <44226762+jamesChiChan@users.noreply.github.com>
2018-12-14 17:06:06 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+// 彈跳視窗出'跟著謙哥吃美食'
+var myHeading = '跟著謙哥吃美食';
+alert(myHeading);
+//只要點擊視窗內任何地方都會彈跳視窗
+//document.querySelector('html').onclick = function() {
+//    alert('Ouch! Stop poking me!');
+//}
+// 圖片轉換 但只影響第一張圖
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/IMG_2678.JPG') {
+      myImage.setAttribute ('src','images/IMG_2677.JPG');
+    } else {
+      myImage.setAttribute ('src','images/IMG_2678.JPG');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading1 = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading1.textContent = '歡迎來到謙哥的美食日記, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading1.textContent = '歡迎來到謙哥的美食日記, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
